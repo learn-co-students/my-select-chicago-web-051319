f0effae0096a77221221096bca48b13bcc1a743a
Emily R <emilyroulund@gmail.com>
2019-04-24 03:33:46 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
+# def my_select(collection)
+# collection.select {|item| item.even? }
+# end
+# this code passed but I don't think it's the answer they're looking for 
+
 def my_select(collection)
- collection.select {|item| item.even? }
+  i = 0
+  select = []
+  while i < collection.length
+    if yield(collection[i])
+      select << collection[i]
+    end
+    i+=1
+  end
+  select
 end
